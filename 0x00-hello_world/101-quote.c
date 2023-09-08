920f46567d7fa86cbba09eb3920c94eaceddc0e9
Rodern <alainkimbu@gmail.com>
2023-09-08 00:18:20 -0700

    fixed new line issues and returned 1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,12 +9,12 @@ int main(void)
 {
 	const char *str1 = "and that piece of art is useful";
 	const char q = '"';
-	const char *str2 = " - Dora Korpar, 2015-10-19";
+	const char *str2 = " - Dora Korpar, 2015-10-19\n";
 	const int str_length1 = 31;
-	const int str_length2 = 26;
+	const int str_length2 = 28;
 	/* Printing the string using fwrite */
 	fwrite(str1, sizeof(char), str_length1, stdout);
 	putchar(q);
 	fwrite(str2, sizeof(char), str_length2, stdout);
-	return (0);
+	return (1);
 }
