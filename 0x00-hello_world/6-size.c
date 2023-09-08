2e9381f671b8ae99c4e0f544064b241ec622e1cc
Rodern <alainkimbu@gmail.com>
2023-09-07 22:52:16 -0700

    lot of fixes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * file_name: 6-size.c
+ * main - Entry point
+ * Description: Program to print the size of variables of different types
+ * Return: Always 0 (Success)
+ * Author: Alain Kimbu
+ */
+int main(void)
+{
+	/* Declaring various variables */
+	char a;
+	int b;
+	long int c;
+	long long d;
+	float e;
+	/* Printing sizes of variables */
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(e));
+	return (0);
+}
