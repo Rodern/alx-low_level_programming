519557d47673109701b36d6e2d820a6588333a16
Rodern <alainkimbu@gmail.com>
2023-09-08 14:48:13 -0700

    fixes

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Always 0 (Success)
+ */
+int main(void)
+{
+	/* variable for loop */
+	int i = 97;
+
+	/* starting from 97 to 122, are all lowercase letters */
+	do {
+		putchar(i);
+		putchar('\n');
+		i++;
+	} while (i < 122);
+	return (0);
+}
