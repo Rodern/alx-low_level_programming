cc767284ef0cb8bb7536548603950ebb44f770a8
Rodern <alainkimbu@gmail.com>
2023-09-08 00:09:08 -0700

    ADD 101-main.c

---
+++ 0x00-hello_world/101-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	const char *str1 = "and that piece of art is useful";
+	const char q = '"';
+	const char *str2 = " - Dora Korpar, 2015-10-19";
+	const int str_length1 = 31;
+	const int str_length2 = 26;
+	/* Printing the string using fwrite */
+	fwrite(str1, sizeof(char), str_length1, stdout);
+	putchar(q);
+	fwrite(str2, sizeof(char), str_length2, stdout);
+	return (0);
+}
