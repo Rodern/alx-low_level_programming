e8759c0a25ed60ddb5a87d180acbb1371ebaf5e9
Rodern <alainkimbu@gmail.com>
2023-09-08 17:44:45 -0700

    UPDATE 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,13 +8,13 @@
 int main(void)
 {
 	/* variable for loop */
-	int i = 97;
+	int i = 16;
 
-	/* starting from 97 to 122, are all lowercase letters */
+	/* starting from 16 down to 0, are all hexadeximal numbers in base 16*/
 	do {
-		putchar(i);
-		i++;
-	} while (i < 122);
+		putchar(i + '0');
+		i--;
+	} while (i < 0);
 	putchar('\n');
 	return (0);
 }
