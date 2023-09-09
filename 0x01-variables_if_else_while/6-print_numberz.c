ad245206e217d9fa32a1c20d45bed7c757a8d48e
Rodern <alainkimbu@gmail.com>
2023-09-08 17:19:19 -0700

    UPDATE 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* variable for loop */
+	int i = 0;
+
+	/* starting from 0, print all single digits */
+	do {
+		putchar(i + '0');
+		i++;
+	} while (i < 10);
+	putchar('\n');
+	return (0);
+}
