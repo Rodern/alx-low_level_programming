68de7bd2b2e44dfd9cfa51c38710ec01d2b543c5
Rodern <alainkimbu@gmail.com>
2023-09-13 23:53:14 -0700

    check

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,31 +1,9 @@
 #include "main.h"
 
-/* function prototype */
-void print_alphabet(void);
-#include "main.h"
-
-/* function prototype */
-void print_alphabet(void);
-
-/**
- * print_alphabet - function
- *
- * Return: Always 0 (Success)
- */
-void print_alphabet(void)
-{
-	int i = 97;
-
-	do {
-		_putchar(i);
-		i++;
-	} while (i < 123);
-}
-
 /**
  * main - entry point
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,9 @@
-#ifndef _PUTCHAR_H
-#define _PUTCHAR_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <unistd.h>
 
 int _putchar(char c);
+void print_alphabet(void);
 
-#endif /* _PUTCHAR_H */
+#endif /* MAIN_H */
