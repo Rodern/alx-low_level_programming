b981e8c71c160fdeb4d8c789e691047c3215101f
Rodern <alainkimbu@gmail.com>
2023-09-14 00:17:36 -0700

    changed main.c

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -25,15 +25,3 @@ void print_alphabet(void)
 		i++;
 	} while (i < 123);
 }
-
-/**
- * main - entry point
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet();
-	_putchar('\n');
-	return (0);
-}
