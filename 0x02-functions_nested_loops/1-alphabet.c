02971ee2e645c70d61224cfa519f90502354d9bb
Rodern <alainkimbu@gmail.com>
2023-09-21 05:24:29 -0700

    fixation check

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
 /**
+ * print_alphabet - function
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int i = 97;
+
+	do {
+		_putchar(i);
+		i++;
+	} while (i < 123);
+}
+
+/**
  * main - entry point
  *
  * Return: Always 0
