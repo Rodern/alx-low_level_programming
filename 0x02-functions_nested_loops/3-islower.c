eca04b23af635541749f1ea2a6fa54d77633f7be
Rodern <alainkimbu@gmail.com>
2023-09-21 07:15:49 -0700

    ADD 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,14 +3,14 @@
 /**
  * _islower - function
  *
- * Return: int
+ * Return: (int)
  */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-		return 1;
+		return (1);
 	}
-	return 0;
+	return (0);
 }
 
