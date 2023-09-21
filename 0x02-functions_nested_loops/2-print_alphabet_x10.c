0f2e4fa3884839225b5efa19fe517200989bf28c
Rodern <alainkimbu@gmail.com>
2023-09-21 06:57:42 -0700

    ADD 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i = 97, j = 0;
+
+	while(j < 10) {
+		do {
+			_putchar(i);
+			i++;
+		} while (i < 123);
+		_putchar('\n');
+		j++;
+	}
+}
+
