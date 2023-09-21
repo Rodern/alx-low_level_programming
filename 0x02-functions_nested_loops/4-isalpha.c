ae2e126cc2d3f5108ead18e3a7156d95ccb6873d
Rodern <alainkimbu@gmail.com>
2023-09-21 07:25:29 -0700

    ADD 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - function
+ * @c: The int to be checked
+ *
+ * Return: (int)
+ */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 || c >= 65 && <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
+
