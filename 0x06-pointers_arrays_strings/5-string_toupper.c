fa50347f3b4d1de0ae6f8e51a2d2af05b285c116
Rodern <alainkimbu@gmail.com>
2023-09-22 16:05:45 -0700

    ADD 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include <string.h>
+
+/**
+ * string_toupper - function
+ * @a: characters
+ *
+ * Return: (char *)
+ */
+char *string_toupper(char *a)
+{
+	int i = 0;
+	char *s = malloc(sizeof(char) * strlen(a));
+
+	for (i = 0; i <= strlen(a) - 1; i++)
+	{
+		if (i == strlen(a) - 1)
+		{
+			s[i] = a[i];
+		}
+		else
+		{
+			s[i] = toupper(a[i]);
+		}
+	}
+	return (s);
+}
