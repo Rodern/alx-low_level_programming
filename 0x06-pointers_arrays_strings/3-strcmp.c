5a6f5e132433bd5b98b773f5ed0e2878f17f101d
Rodern <alainkimbu@gmail.com>
2023-09-22 13:02:36 -0700

    ADD 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include <string.h>
+
+/**
+ * _strcmp - function
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: (int)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int s = 0;
+
+	s = strcmp(s1, s2);
+	return (s);
+}
