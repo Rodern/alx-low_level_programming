0e97fd21b8c84b0bf957f8f667ad8ce4c7cdb6f6
Rodern <alainkimbu@gmail.com>
2023-09-22 18:04:48 -0700

    kjalsfhdsjhfl

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,14 +11,16 @@
 char *cap_string(char *a)
 {
 	int i = 0;
+	char *s = malloc(sizeof(char) * strlen(a));
 
+	strcpy(s, a);
 	for (i = 0; a[i]; i++)
 	{
 		if (a[i] == ' ' || a[i] == '.' || a[i] == '-' || a[i] == '_' || a[i] == '\n'
 				|| a[i] == '\t' || (isdigit(a[i]) && (a[i + 1] >= 97 && a[i + 1] <= 122)))
 		{
-			a[i + 1] = toupper(a[i + 1]);
+			s[i + 1] = toupper(a[i + 1]);
 		}
 	}
-	return (a);
+	return (s);
 }
